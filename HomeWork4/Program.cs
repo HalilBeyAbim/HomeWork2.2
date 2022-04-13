a41fc3ed8d404925de03a5667ff11296215662d6
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-14 00:37:17 +0400

    task2

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir reqem daxil edin");
+            string s = Console.ReadLine();
+            int i = 0;
+            int count = s.Length;
+            int sum = 0;
+            while (i < count)
+            {
+                if (char.IsDigit(s[i]))
+                    sum += s[i++] - 48;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
